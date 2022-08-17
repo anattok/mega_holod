2f6e6ede21256297c221e449b0b8396a774693ba
anattok <tokarev182@yandex.ru>
2022-08-17 22:55:01 +0300

    Сделал первые два блока

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+const headerSlider = document.querySelector('.swiper');
+
+
+const mySwiper = new Swiper (headerSlider, {
+
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+});
+
+
+
+
+
+
+
+
+
+
+
+
