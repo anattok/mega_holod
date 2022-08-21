b0a1c091fc5616b417f9cb0c37ce88e96463b859
anattok <tokarev182@yandex.ru>
2022-08-21 18:01:38 +0300

    Сделал открытие категорий по нажатию в сайдбаре

--- app/js/main.js
+++ app/js/main.js
@@ -24,25 +24,8 @@ const mySwiperTwo = new Swiper(partnersSlider, {
 
 });
 
-// УПАРАВЛЕНИЕ САЙДБАРОМ В КАТАЛОГЕ
-const navHeader = document.querySelectorAll('.nav-item__header');
 
-navHeader.forEach((item => {
-
-  item.addEventListener('click', function (event) {
-
-    const list = event.target.parentElement.lastElementChild;
-    const header = event.target;
-
-    list.classList.toggle('active');
-    header.classList.toggle('active');
-
-
-  });
-
-}));
-
-
+//ДАННЫЕ ТОВАРОВ
 
 let holod = {
   "1": {
@@ -110,7 +93,7 @@ let pekar = {
   },
 };
 
-torg = {
+let torg = {
   "1": {
     "name": "Кондитерский стеллаж",
     "link": "product.html",
@@ -138,35 +121,203 @@ torg = {
     "volt": "220v",
     "temp": "+1 - +10C"
   },
+  "4": {
+    "name": "ааавтомобиль",
+    "link": "product.html",
+    "img": "https://s0.rbk.ru/v6_top_pics/media/img/8/25/756462167448258.jpg",
+    "brand": "Бренд 3",
+    "size": "20х30х500мм",
+    "volt": "220v",
+    "temp": "+1 - +10C"
+  },
+};
+
+let asideMenu = {
+  'Холодильное оборудование' : ['Лари морозильные', 'Витрины кондитерские', 'Витрины низкотемпературные' ],
+  'Оборудование для пекарни' : ['Оборудование для резки', 'Оборудование раскатки теста', 'Оборудование для расстойки'],
+  'Торговое оборудование' : ['Кондитерские стеллажи', 'Торговые стеллажи', 'Другие стеллажи' ]
 };
 
 
-
-/* let out = "";
-
-for (let key in holod) {
-
-  let itemImage = document.querySelector('.product__image');
-  let itemTitle = document.querySelector('.product__title');
-  let itemMore = document.querySelector('.product__more');
-  let itemPrice = document.querySelector('.product__price');
-
-  itemImage += holod[key].name;
-  itemTitle += holod[key].img;
-  itemMore += holod[key].link;
-}
-
-const item = document.querySelectorAll('.product__card');
+ //ВСЕ ВЫПАДАШКИ
+ const allListAside = document.querySelectorAll('.nav-item__list');
+ //ВСЕ item В ВЫПАДАШКЕ
+ const allItemAside = document.querySelectorAll('.nav-item__item');
 
 
-itemTitle.forEach(item => {
-  item.innerHTML = out;
-})
 
 
-localStorage.setItem('holod', JSON.stringify(holod));
-localStorage.setItem('pekar', JSON.stringify(pekar));
-localStorage.setItem('torg', JSON.stringify(torg)); */
+
+
+
+
+
+
+
+
+
+
+
+
+//ПСЕВДОМАССИВ ВСЕХ ЗАГОЛОВКОВ САЙДБАРА
+const navHeader = document.querySelectorAll('.nav-item__header');
+//КАТЕГОРИИ В САЙДБАРЕ
+const fridge = document.querySelector('.category-fridge');
+const bakery = document.querySelector('.category-bakery');
+const trade = document.querySelector('.category-trade');
+
+
+//СОЗДАНИЕ ХЛЕБНЫХ КРОШЕК
+const breadcrumpsList = document.querySelector('.breadcrumps__list');
+const breadcrumpsItems = document.querySelectorAll('.breadcrumps__item');
+
+
+
+
+// УПАРАВЛЕНИЕ САЙДБАРОМ В КАТАЛОГЕ
+//ПЕРЕБЕРЁМ ФОРИЧЕМ ВСЕ ЗАГОЛОВКИ В САЙДБАРЕ КУДА МОЖНО НАЖАТЬ
+navHeader.forEach((item => {
+
+  item.addEventListener('click', function (event) {
+
+    //list ПРИСВАИВАЕМ ПОСЛЕДНИЙ ЭЛЕМЕНТ РОДИТЕЛЬСКОГО ЭЛЕМЕНТА ЗАГОЛОВКА ПО КОТОРОМУ КЛИКНУЛИ (это улка)
+    const list = event.target.parentElement.lastElementChild;
+    //header ПРИСВАИВАЕМ ДИВ НА КОТОРЫЙ НАЖАЛИ
+    const header = event.target;
+
+
+   
+
+    //УДАЛИМ У ВСЕХ КАТЕГОРИЙ-ВЫПАДАШЕК КЛАСС ACTIVE ЧТОБЫ ОСТАЛЬНЫЕ ЗАКРЫЛИСЬ
+    allListAside.forEach(item => {
+      item.classList.remove('active');
+
+    });
+
+    //УДАЛИМ У ВСЕХ заголовков КАТЕГОРИЙ-ВЫПАДАШЕК КЛАСС ACTIVE ЧТОБЫ удалилась стрелка вверх
+    navHeader.forEach(headerCategory => {
+      headerCategory.classList.remove('active');
+
+    });
+
+    //ПРИСВАИВАЕМ КЛАСС active чтобы выпадашка появилась
+    header.classList.add('active');
+
+
+
+    //СОЗДАНИЕ ВЁРСТКИ КАРТОЧЕК ТОВАРОВ
+    let itemCards = document.querySelector('.product__cards');
+
+
+    if (header == fridge) {
+      itemCards.innerHTML = "";
+      list.classList.add('active');
+
+      //ПЕРЕБОР item В ВЫПАДАШКЕ
+      allItemAside.forEach((item => {
+
+        item.addEventListener('click', function (event) {
+      
+          const navSubHeader = event.target;
+          console.log(navSubHeader);
+      
+      
+        });
+      }));
+
+
+      for (let key in holod) {
+
+        itemCards.innerHTML += `
+          
+          <li class="product__card">
+              <div class="product__image"><img src="${holod[key].img}" alt=""></div>
+              <h6 class="product__title">${holod[key].name}</h6>
+              <div class="product__bottom">
+                  <a href="product.html" class="product__more">Подробнее</a>
+                  <a href="#" class="product__price">Узнать цену</a>
+              </div>
+          </li>
+          
+          `;
+
+      }
+
+    } else if (header == bakery) {
+
+      itemCards.innerHTML = "";
+      list.classList.add('active');
+
+      breadcrumpsList.insertAdjacentHTML("beforeend",
+
+        `<li class="breadcrumps__item">
+          <a class="breadcrumps__link" href="catalog.html">Оборудование для пекарни</a>
+      </li>
+
+    `);
+
+      for (let key in pekar) {
+
+        itemCards.innerHTML += `
+          
+          <li class="product__card">
+              <div class="product__image"><img src="${pekar[key].img}" alt=""></div>
+              <h6 class="product__title">${pekar[key].name}</h6>
+              <div class="product__bottom">
+                  <a href="product.html" class="product__more">Подробнее</a>
+                  <a href="#" class="product__price">Узнать цену</a>
+              </div>
+          </li>
+          
+          `;
+
+      }
+    } else if (header == trade) {
+
+      itemCards.innerHTML = "";
+      list.classList.add('active');
+
+      for (let key in torg) {
+
+        itemCards.innerHTML += `
+          
+          <li class="product__card">
+              <div class="product__image"><img src="${torg[key].img}" alt=""></div>
+              <h6 class="product__title">${torg[key].name}</h6>
+              <div class="product__bottom">
+                  <a href="product.html" class="product__more">Подробнее</a>
+                  <a href="#" class="product__price">Узнать цену</a>
+              </div>
+          </li>
+          
+          `;
+
+      }
+    }
+
+
+  });
+
+}));
+
+
+
+
+
+
+
+/* 
+breadcrumpsList.insertAdjacentHTML("beforeend",
+  `<li class="breadcrumps__item">
+              <a class="breadcrumps__link" href="catalog.html">Холодильное оборудование</a>
+          </li>
+        `); */
+
+
+
+
+
+
 
 
 
